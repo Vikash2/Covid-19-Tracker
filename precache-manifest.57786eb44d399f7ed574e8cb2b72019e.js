79c6afb44d24b4bd45d9c4bfd5a0a9a0612d5a46
Vikash Kumar Pandit <kumar.vikash1109@gmail.com>
2020-04-21 10:43:41 +0530

    Updates

---
+++ precache-manifest.57786eb44d399f7ed574e8cb2b72019e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61cf1727f2eeba97695dbb125cd2a7c",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "9f0321148947758e2f3e",
+    "url": "/Covid-19-Tracker/static/css/main.22c70948.chunk.css"
+  },
+  {
+    "revision": "09df8453ec53118a2a75",
+    "url": "/Covid-19-Tracker/static/js/2.4efb8188.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.4efb8188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0321148947758e2f3e",
+    "url": "/Covid-19-Tracker/static/js/main.0c35a541.chunk.js"
+  },
+  {
+    "revision": "27c9da7093597dbbbd46",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.84d986c2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/covidimage.d7265326.png"
+  }
+]);
